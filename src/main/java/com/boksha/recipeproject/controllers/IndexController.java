886108c71c4b03c0b914268403a66b34f429835f
Bojan Sutulovic <bsutulovic@novomaticls.com>
2019-04-29 09:49:10 +0200

    Use Spring data JPA Query methods

--- src/main/java/com/boksha/recipeproject/controllers/IndexController.java
+++ src/main/java/com/boksha/recipeproject/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.boksha.recipeproject.controllers;
 
+import com.boksha.recipeproject.domain.Category;
+import com.boksha.recipeproject.domain.UnitOfMeasure;
+import com.boksha.recipeproject.repositories.CategoryRepository;
+import com.boksha.recipeproject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/com/boksha/recipeproject/repositories/CategoryRepository.java
+++ src/main/java/com/boksha/recipeproject/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.boksha.recipeproject.repositories;
 import com.boksha.recipeproject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+  Optional<Category> findByDescription(String description);
+
 }

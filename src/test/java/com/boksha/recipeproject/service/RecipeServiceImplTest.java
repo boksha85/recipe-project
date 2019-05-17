5b928cd1ebb2b382c7adc04a2ce1107051653b5f
Bojan Sutulovic <bsutulovic@novomaticls.com>
2019-05-17 23:35:52 +0200

    Simple using of Mockito

---
+++ src/test/java/com/boksha/recipeproject/service/RecipeServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.boksha.recipeproject.service;
+
+import com.boksha.recipeproject.domain.Recipe;
+import com.boksha.recipeproject.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class RecipeServiceImplTest {
+
+  RecipeServiceImpl recipeService;
+  @Mock
+  RecipeRepository recipeRepository;
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    recipeService = new RecipeServiceImpl(recipeRepository);
+  }
+
+  @Test
+  public void getRecipes() {
+    Recipe recipe = new Recipe();
+    HashSet recipeData = new HashSet();
+    recipeData.add(recipe);
+
+    Mockito.when(recipeRepository.findAll()).thenReturn(recipeData);
+
+    Set<Recipe> recipes = recipeService.getRecipes();
+    assertEquals(recipes.size(), 1);
+    Mockito.verify(recipeRepository, Mockito.times(1)).findAll();
+  }
+}
